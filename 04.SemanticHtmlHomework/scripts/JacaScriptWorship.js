3d89f6d81972dc12cf839cb7cdfbf8f5bb2a411f
StefanBratovanov <bratovanovstefan@gmail.com>
2015-03-07 10:31:32 +0200

    SoftUni HTML CSS course

---
+++ 04.SemanticHtmlHomework/scripts/JacaScriptWorship.js
@@ -0,0 +1,9 @@
+var context = document.getElementById("canvas").getContext("2d");
+context.font = "bold 32px tahoma";
+context.textAlign = "center";
+context.fillText("I love JavaScript", 150, 50);
+context.fillStyle = "#F1DA4E";
+context.fillRect(70, 80, 160, 160);
+context.font = "bold 80px tahoma";
+context.fillStyle = "black";
+context.fillText("JS", 170, 220);
